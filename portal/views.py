af0efb20b0d50bb32b06d8f4ae32c5a0536ae462
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-04-26 01:34:06 +0300

    add docx file too

--- portal/views.py
+++ portal/views.py
@@ -1,10 +1,11 @@
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from .serializers import CustomUserSerializer, ResourceSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from PyPDF2 import PdfReader
 from .models import Resource, CustomUser
 import os
+from docx import Document
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,7 +21,6 @@ class ResourceViewSet(viewsets.ModelViewSet):
     def metadata(self, request, pk=None):
         try:
             file_obj = self.get_object().file
-
             metadata = {'type': os.path.splitext(file_obj.name)[1][1:].upper(),
                         'size': round(file_obj.size / (1024 * 1024), 2)}
 
@@ -30,9 +30,13 @@ class ResourceViewSet(viewsets.ModelViewSet):
                     metadata['title'] = pdf.docinfo.title if hasattr(pdf,
                                                                      'docinfo') and pdf.docinfo else os.path.basename(
                         file_obj.name)
+            elif file_obj.name.endswith('.docx'):
+                doc = Document(file_obj)
+                metadata['title'] = doc.core_properties.title if doc.core_properties.title else os.path.basename(
+                    file_obj.name)
 
             return Response(metadata)
         except Resource.DoesNotExist:
-            return Response({'error': 'Resource not found'}, status=404)
+            return Response({'error': 'Resource not found'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return Response({'error': str(e)}, status=500)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
