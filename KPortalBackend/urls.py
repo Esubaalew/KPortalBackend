1bc551585c0764738a268f7b8b96f2cd222ff495
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-05-04 11:55:49 +0300

    "" now moves to /api/

--- KPortalBackend/urls.py
+++ KPortalBackend/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('portal.urls')),
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
