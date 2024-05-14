051e06009d29defd47c6a52f9045706ac9d67a8b
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-05-14 11:37:53 +0300

    makde fake followign

--- fill_languages.py
+++ fill_languages.py
@@ -4,7 +4,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KPortalBackend.settings")
 django.setup()
 
-from portal.models import Language
+from portal.models import Language, Follow
 import random
 from faker import Faker
 from django.utils import timezone
@@ -17,32 +17,29 @@ from django.utils import timezone
 from portal.models import Resource, CustomUser, Like
 
 
-def populate_random_likes(num_likes):
-    # Get all resources and users
-    resources = Resource.objects.all()
+def populate_random_follows(num_follows):
+    # Get all users
     users = CustomUser.objects.all()
 
-    # Calculate the total number of resources and users
-    num_resources = len(resources)
+    # Calculate the total number of users
     num_users = len(users)
 
-    # Iterate to create random likes
-    for _ in range(num_likes):
-        # Randomly select a resource and user
-        random_resource = random.choice(resources)
-        random_user = random.choice(users)
+    # Iterate to create random follows
+    for _ in range(num_follows):
+        # Randomly select a follower and a followed user
+        random_follower = random.choice(users)
+        random_followed_user = random.choice(users.exclude(id=random_follower.id))
 
-        # Check if the user has already liked the resource
-        if not Like.objects.filter(resource=random_resource, user=random_user).exists():
-            # Create a like object
-            like = Like.objects.create(
-                resource=random_resource,
-                user=random_user,
-                date_liked=timezone.now()
+        # Check if the follow already exists
+        if not Follow.objects.filter(follower=random_follower, followed_user=random_followed_user).exists():
+            # Create a follow object
+            follow = Follow.objects.create(
+                follower=random_follower,
+                followed_user=random_followed_user,
+                date_followed=timezone.now()
             )
-            # Optionally, you can print the created like object
-            print(f"Created like: {like}")
+            # Optionally, you can print the created follow object
+            print(f"Created follow: {follow}")
 
-
-# Example usage: Populate 100 random likes
-# populate_random_likes(50)
+# Example usage: Populate 100 random follows
+# populate_random_follows(100)
